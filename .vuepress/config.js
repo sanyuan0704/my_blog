3174c69c2ca852fdbdd36d637b1de81432216cb5
sanyuan0704 <494130947@qq.com>
2021-10-09 20:52:00 +0800

    chore: update github link

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,98 +1,93 @@
-const sidebar = require('./siderbar.js');
+const sidebar = require("./siderbar.js");
 module.exports = {
-  "title": "三元博客",
-  "description": "三元同学的博客",
-  "dest": "public",
+  title: "三元博客",
+  description: "三元同学的博客",
+  dest: "public",
   // "base": '/my_blog/',
-  "base": "/",
-  "head": [
+  base: "/",
+  head: [
     [
       "link",
       {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
+        rel: "icon",
+        href: "/favicon.ico",
+      },
     ],
     [
       "meta",
       {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
   ],
-  "plugins": ["@vuepress-reco/vuepress-plugin-comments", "vuepress-plugin-meting"],
-  "theme": "reco",
-  "themeConfig": {
-    "mode": 'light',
-    "subSidebar": 'auto',
-    "valineConfig": {
-      "appId": 'h6i7vXvVEK37fFFwb1DtMJ6W-gzGzoHsz',
-      "appKey": 'cYI3hypnGAI00CPuzhNfeY8Q',
-    },  
-    "nav": [
+  plugins: [
+    "@vuepress-reco/vuepress-plugin-comments",
+    "vuepress-plugin-meting",
+  ],
+  theme: "reco",
+  themeConfig: {
+    mode: "light",
+    subSidebar: "auto",
+    valineConfig: {
+      appId: "h6i7vXvVEK37fFFwb1DtMJ6W-gzGzoHsz",
+      appKey: "cYI3hypnGAI00CPuzhNfeY8Q",
+    },
+    nav: [
       {
-        "text": "主页",
-        "link": "/",
-        "icon": "reco-home"
+        text: "主页",
+        link: "/",
+        icon: "reco-home",
       },
       {
-        "text": "时间线",
-        "link": "/timeline/",
-        "icon": "reco-date"
+        text: "时间线",
+        link: "/timeline/",
+        icon: "reco-date",
       },
-      // {
-      //   "text": "Docs",
-      //   "icon": "reco-message",
-      //   "items": [
-      //     {
-      //       "text": "vuepress-reco",
-      //       "link": "/docs/theme-reco/"
-      //     }
-      //   ]
-      // },
       {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
+        text: "Contact",
+        icon: "reco-message",
+        items: [
           {
-            "text": "GitHub",
-            "link": "https://github.com/recoluan",
-            "icon": "reco-github"
-          }
-        ]
-      }
+            text: "GitHub",
+            link: "https://github.com/sanyuan0704/my_blog",
+            icon: "reco-github",
+          },
+          {
+            text: "掘金",
+            link: "https://juejin.cn/user/430664257382462",
+            icon: "reco-juejin",
+          },
+          {
+            text: "知乎",
+            link: "https://www.zhihu.com/people/yang-xing-yuan-9",
+            icon: "reco-zhihu",
+          },
+        ],
+      },
     ],
     sidebar,
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "目录索引"
+    type: "blog",
+    blogConfig: {
+      category: {
+        location: 2,
+        text: "目录索引",
       },
-      "tag": {
-        "location": 3,
-        "text": "标签索引"
-      }
+      tag: {
+        location: 3,
+        text: "标签索引",
+      },
     },
-    "friendLink": [
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      }
-    ],
-    "logo": "/logo.png",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "lastUpdated": "Last Updated",
-    "author": "sanyuan",
-    "authorAvatar": "/avatar.jpg",
-    "record": "xxxx",
-    "startYear": "2020"
+    logo: "/logo.png",
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: "Last Updated",
+    author: "sanyuan",
+    authorAvatar: "/avatar.jpg",
+    record: "xxxx",
+    startYear: "2020",
   },
-  "markdown": {
-    "lineNumbers": true
-  }
-}
+  markdown: {
+    lineNumbers: true,
+  },
+};
--- README.md
+++ README.md
@@ -11,17 +11,8 @@ tagline: 认真写点儿东西
 #   background: '#fff',
 #   borderRadius: '1rem',
 # }
-bgImageStyle: {
-  height: '450px'
-}
+bgImageStyle: { height: "450px" }
 isShowTitleInHome: false
 actionText: Guide
 actionLink: /views/other/guide
-features:
-- title: Yesterday
-  details: 开发一款看着开心、写着顺手的 vuepress 博客主题
-- title: Today
-  details: 希望帮助更多的人花更多的时间在内容创作上，而不是博客搭建上
-- title: Tomorrow
-  details: 希望更多的爱好者能够参与进来，帮助这个主题更好的成长
 ---
